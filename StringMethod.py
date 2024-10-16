c7977bebd9655be01caef85033e5f362c464893b
MrunalWakadkar <wakadkarmrunal@gmail.com>
2024-10-17 00:45:18 +0530

    Added stringmethod

---
+++ StringMethod.py
@@ -0,0 +1,12 @@
+str="my is name is mrunal"
+
+print(str.capitalize())
+print(str.upper())
+print(str.casefold())
+print(str.lower())
+
+print(str.count("m"))
+
+print(str.encode())
+print(str.find("mrunal"))
+print(str.split())
