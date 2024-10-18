2cd4a31a02d1e3e5f1dfd6ae9ffbb212cc1de07c
MrunalWakadkar <wakadkarmrunal@gmail.com>
2024-10-18 21:39:32 +0530

    Added multiplication generator

--- Student_Management_System.py
+++ Student_Management_System.py
@@ -1,17 +1,25 @@
 Details ={"sita":{"age":20 ,"marks":90} , "gita":{"age":19 ,"marks":75} , "mita":{"age":22 , "marks":88},"tina":{"age":16 ,"marks":65} ,"nita":{"age":19 ,"marks":85}
           }
 
-choice=input("Enter your choice: ")
-if(choice==1):
-    def Addentry():
+#exitchoice=input("Enter your choice: ")
+
+def Addentry():
     name=input("Enter name of the student : ")
     age=int(input("Enter age :"))
     marks=int(input("Enter marks :"))
-    Details[name]={age,marks}
+    Details[name1]={age,marks}
 
-elif(choice==2):
-    def updateentry():
-    name=input("Enter name of the student which you want to update  : ")
+def updateentry():
+    name2=input("Enter name of the student which you want to update  : ")
     age=int(input("Enter updated age :"))
     marks=int(input("Enter updated marks :"))
-    Details.update({name:{age.marks}})
+    Details.update({name2:{age,marks}})
+
+Addentry()
+updateentry()
+
+    
+
+
+
+
